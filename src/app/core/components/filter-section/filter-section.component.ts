a171d06baa285efd7953a92313a58a45e24ce055
Fuzzy-Fluffy-Cat <christian_mir_g_castillo@dlsu.edu.ph>
2023-10-27 17:22:40 +0800

    Finished Filter Design Layout
    - Filter Section design revised
    - Added "Filter Type" Button and dropdown list for "Filter Type"
    - Added list of filters for each filter type
    - Possibly fixed the layout of the map

--- src/app/core/components/filter-section/filter-section.component.ts
+++ src/app/core/components/filter-section/filter-section.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter-section.component.css']
 })
 export class FilterSectionComponent {
-
+  onClick() {
+    alert("Filter Out Some Stuff");
+  }
 }
